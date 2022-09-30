26758576ab5daf08d134556257a91165cce7cfe3
Vladimir Botalov <vovan61911@gmail.com>
2022-09-30 15:23:41 +0400

    Сохранение перед добавлением sass.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./images/logo.jpg"
+import "./main.css";
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  <div>
+      <header>
+        <img width="112px" height="112px" src={logo}></img>
+        <nav className="menu__body">
+          <ul className="menu__list">
+              <li><a className="menu__link">Главная</a></li>
+              <li><a className="menu__link">Главная</a></li>
+              <li><a className="menu__link">Главная</a></li>
+          </ul>
+        </nav>
       </header>
-    </div>
+  </div>
   );
 }
 
